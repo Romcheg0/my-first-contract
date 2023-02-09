41d0843e1e57e6ba135326a1eac054c11dc672f7
Roman <roman.onyshchuk@nure.ua>
2023-02-09 14:54:42 +0200

    Fixed the issues

--- contracts/MyFirstContract.sol
+++ contracts/MyFirstContract.sol
@@ -1,22 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyFirstContract is ERC20 {
-
     constructor(uint256 initialSupply) ERC20 ("MyFirstContract", "MFC") {
         _mint(msg.sender, initialSupply);
     }
     
     function makeTransfers(address payable[] memory addresses, uint256[] memory amounts) public {
-        if(addresses.length == amounts.length && addresses.length != 0){
-            for(uint i = 0; i < addresses.length; i++){
-                if(totalSupply() >= amounts[i]){
-                    _transfer(msg.sender, addresses[i], amounts[i]);
-                }
-            }
+        require(addresses.length == amounts.length, 'Different lengths of arrays!');
+        require (addresses.length != 0, 'Empty array!');
+        for(uint i = 0; i < addresses.length; i++){
+            _transfer(msg.sender, addresses[i], amounts[i]);
         }
     }
 }
---
+++ test/MyFirstContract.js
@@ -0,0 +1,91 @@
+let { expect } = require('chai')
+describe('Initial balance & reverts: ', function () {
+	let MyFirstContract
+	let myFirstContract
+	before(async () => {
+		MyFirstContract = await ethers.getContractFactory('MyFirstContract')
+		return (myFirstContract = await MyFirstContract.deploy(1000000))
+	})
+	it('Should perform an initial balance', async () => {
+		expect(
+			await myFirstContract.balanceOf(
+				'0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266'
+			)
+		).to.be.equal(1000000)
+	})
+	it('Should revert on empty array(s)', async () => {
+		await expect(
+			myFirstContract.makeTransfers([], [1, 2, 3])
+		).to.be.revertedWith('Different lengths of arrays!')
+	})
+	it('Should revert on different sizes of arrays', async function () {
+		const MyFirstContract = await ethers.getContractFactory('MyFirstContract')
+		const myFirstContract = await MyFirstContract.deploy(1000000)
+		await expect(
+			myFirstContract.makeTransfers(
+				[
+					'0x1be1faf129e8ce0b9bb633db66876df6c42acac2',
+					'0x1be1faf129e8ce0b9bb633db66876df6c42acac2',
+				],
+				[1]
+			)
+		).to.be.revertedWith('Different lengths of arrays!')
+	})
+	it('Should reject on bad parameters', async () => {
+		await expect(myFirstContract.makeTransfers(['a'], true)).to.be.rejected
+	})
+})
+describe('Transactions: ', () => {
+	beforeEach(async () => {
+		MyFirstContract = await ethers.getContractFactory('MyFirstContract')
+		return (myFirstContract = await MyFirstContract.deploy(1000000))
+	})
+	it('Should work # 1', async function () {
+		let initialBalance = await myFirstContract.balanceOf(
+			myFirstContract.signer.address
+		)
+		myFirstContract.makeTransfers(
+			['0x1be1faf129e8ce0b9bb633db66876df6c42acac2'],
+			[600000]
+		)
+		await expect(
+			await myFirstContract.balanceOf(myFirstContract.signer.address)
+		).to.be.equal(initialBalance - 600000)
+		await expect(
+			await myFirstContract.balanceOf(
+				'0x1be1faf129e8ce0b9bb633db66876df6c42acac2'
+			)
+		).to.be.equal(600000)
+	})
+	it('Should work # 2', async function () {
+		let initialBalance = await myFirstContract.balanceOf(
+			myFirstContract.signer.address
+		)
+		myFirstContract.makeTransfers(
+			[
+				'0x1be1faf129e8ce0b9bb633db66876df6c42acac2',
+				'0x1be1faf129e8ce0b9bb633db66876df6c42acac3',
+				'0x1be1faf129e8ce0b9bb633db66876df6c42acac4',
+			],
+			[600000, 100000, 200000]
+		)
+		await expect(
+			await myFirstContract.balanceOf(myFirstContract.signer.address)
+		).to.be.equal(initialBalance - 900000)
+		await expect(
+			await myFirstContract.balanceOf(
+				'0x1be1faf129e8ce0b9bb633db66876df6c42acac2'
+			)
+		).to.be.equal(600000)
+		await expect(
+			await myFirstContract.balanceOf(
+				'0x1be1faf129e8ce0b9bb633db66876df6c42acac3'
+			)
+		).to.be.equal(100000)
+		await expect(
+			await myFirstContract.balanceOf(
+				'0x1be1faf129e8ce0b9bb633db66876df6c42acac4'
+			)
+		).to.be.equal(200000)
+	})
+})
